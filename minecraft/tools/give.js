f500a0021c06ea1a1555c080f603fd43bf635584
Nixinova <Nixinova@users.noreply.github.com>
2019-05-12 10:15:47 +1200

    troubleshoot 6

--- minecraft/tools/give.js
+++ minecraft/tools/give.js
@@ -407,10 +407,11 @@ function give() {
 
             // lore //
             if (i_lore) {
-                i_lore = '\\"' + i_lore.replace(/\\\\/g, '\\').replace(/\\"/g, '\"').replace('\n', '\\",\\"') + '\\"';
+                i_lore = i_lore.replace(/\\\\/g, '\\').replace(/\\"/g, '\"');
                 i_lore = i_lore.split('\n');
                 for (i in i_lore) {
                     i_lore[i] = '\\"' + i_lore[i] + '\\"';
+                    i_lore[i] = i_lore[i].replace(/\\\\/g, '\\');
                 }
                 nbt.display.Lore = i_lore;
             }
