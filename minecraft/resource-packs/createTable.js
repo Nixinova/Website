5ecfdf07dd8d94049b5e7a23084aea1043f50177
Nixinova <Nixinova@users.noreply.github.com>
2019-05-23 18:33:36 +1200

    Use pure JS

---
+++ minecraft/resource-packs/createTable.js
@@ -0,0 +1,10 @@
+function createTable(mcver, arr) {
+    var table = $('#'+mcver);
+    for (i in arr) {
+        table.append(`<tr>
+        <td>Nixinova Mash-Up ${mcver}-${arr.id}</td>
+        <td><a href="https://mediafire.com?${arr.dl}" target="_blank">Download</a></td>
+        <td><small>${arr.date}</small></td>
+        </tr>`)
+    }
+}
