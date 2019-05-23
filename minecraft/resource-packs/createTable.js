1faa0aabb945e4cbfd47ebf5f7d7037581cbe31b
Nixinova <Nixinova@users.noreply.github.com>
2019-05-23 19:05:47 +1200

    Add more tables

--- minecraft/resource-packs/createTable.js
+++ minecraft/resource-packs/createTable.js
@@ -1,11 +1,14 @@
-function createTable(mcver, arr) {
-    var id = mcver.replace(/\./g, '_');
+function createTable(ver, arr) {
+    var id = ver.replace(/\./g, '_');
     var table = $('#'+id);
     for (i in arr) {
         if (arr[i].date === undefined) {arr.date = '';}
+        if (!attr[i].name) {attr[i].name = attr[i].packver;}
+        if (!attr[i].mcver) {attr[i].mcver = ver}
+
         table.append(`<tr>
-        <td style="min-width: 250px">Nixinova Mash-Up ${mcver}-${arr[i].id}</td>
-        <td>${attr[i].packver}</td>
+        <td style="min-width: 250px">Nixinova Mash-Up ${attr[i].mcver} ${arr[i].id}</td>
+        <td>v${attr[i].name}</td>
         <td><a href="https://mediafire.com?${arr[i].dl}" target="_blank">Download</a></td>
         <td><small>${arr[i].date}</small></td>
         </tr>`);
