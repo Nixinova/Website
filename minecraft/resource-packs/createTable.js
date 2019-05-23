11c95846e1d9231c5f2600e2b4d675fae5f5be70
Nixinova <Nixinova@users.noreply.github.com>
2019-05-23 19:23:22 +1200

    Change pre notation

--- minecraft/resource-packs/createTable.js
+++ minecraft/resource-packs/createTable.js
@@ -1,15 +1,15 @@
 function createTable(mcver, arr) {
     var id = mcver.replace(/\./g, '_');
     var table = $('#'+id);
+
     for (i in arr) {
-        if (!arr[i].date) {arr[i].date = '';}
-        if (!arr[i].mcver) {arr[i].mcver = mcver}
+        if (!arr[i].date) {arr[i].date = '(TBA)';}
         arr[i].name = arr[i].name ? arr[i].name.replace('v', '') : arr[i].packver;
 
         table.append(`<tr>
-        <td style="min-width: 250px">Nixinova Mash-Up ${arr[i].mcver} ${arr[i].id}</td>
+        <td style="min-width: 250px">Nixinova Mash-Up ${mcver} (${attr[i].mcver}) ${arr[i].id}</td>
         <td>v${arr[i].name}</td>
-        <td><a href="https://mediafire.com?${arr[i].dl}" target="_blank">Download</a></td>
+        <td width="100px"><a href="https://mediafire.com?${arr[i].dl}" target="_blank">Download</a></td>
         <td><small>${arr[i].date}</small></td>
         </tr>`);
     }
