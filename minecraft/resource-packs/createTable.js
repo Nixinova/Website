669487ef96ff71a018bc72ff2827005b6bf2ff64
Nixinova <Nixinova@users.noreply.github.com>
2019-05-23 18:35:05 +1200

    Add [i]

--- minecraft/resource-packs/createTable.js
+++ minecraft/resource-packs/createTable.js
@@ -2,9 +2,9 @@ function createTable(mcver, arr) {
     var table = $('#v'+mcver);
     for (i in arr) {
         table.append(`<tr>
-        <td>Nixinova Mash-Up ${mcver}-${arr.id}</td>
-        <td><a href="https://mediafire.com?${arr.dl}" target="_blank">Download</a></td>
-        <td><small>${arr.date}</small></td>
+        <td>Nixinova Mash-Up ${mcver}-${arr[i].id}</td>
+        <td><a href="https://mediafire.com?${arr[i].dl}" target="_blank">Download</a></td>
+        <td><small>${arr[i].date}</small></td>
         </tr>`)
     }
 }
