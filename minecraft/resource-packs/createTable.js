1648aaf47aa350ee5c0971c8c36d6be65e229e97
Nixinova <Nixinova@users.noreply.github.com>
2019-05-23 18:42:41 +1200

    Add flex centered

--- minecraft/resource-packs/createTable.js
+++ minecraft/resource-packs/createTable.js
@@ -2,10 +2,11 @@ function createTable(mcver, arr) {
     var id = mcver.replace(/\./g, '_');
     var table = $('#'+id);
     for (i in arr) {
+        if (arr[i].date === undefined) {arr.date = '';}
         table.append(`<tr>
         <td>Nixinova Mash-Up ${mcver}-${arr[i].id}</td>
         <td><a href="https://mediafire.com?${arr[i].dl}" target="_blank">Download</a></td>
         <td><small>${arr[i].date}</small></td>
-        </tr>`)
+        </tr>`);
     }
 }
