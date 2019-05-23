c0ad1b1aa4ceacb5445ad4e6dbc2caf8081c5c35
Nixinova <Nixinova@users.noreply.github.com>
2019-05-23 20:03:08 +1200

    Clean up and fixes

--- minecraft/resource-packs/createTable.js
+++ minecraft/resource-packs/createTable.js
@@ -3,15 +3,15 @@ function createTable1(id, arr) {
 
     for (i in arr) {
         if (!arr[i].date) {arr[i].date = '(TBA)';}
-        arr[i].name = arr[i].name ? arr[i].name.replace('v', '') : arr[i].packver;
-        if (arr[i].pre) {arr[i].mcver += ` (pre${arr[i].pre})`;}
-        if (arr[i].snap) {arr[i].mcver += ` (${arr[i].snap})`;}
+        if (!arr[i].name) {arr[i].name = arr[i].packver;}
+        if (arr[i].pre) {arr[i].mcver == `${arr[i].mcver} (pre${arr[i].pre})`;}
+        if (arr[i].snap) {arr[i].mcver == `${arr[i].mcver} (${arr[i].snap})`;}
 
         table.append(`<tr>
         <td style="min-width: 250px">Nixinova Mash-Up ${arr[i].mcver} ${arr[i].id}</td>
-        <td>v${arr[i].name}</td>
+        <td>${arr[i].name}</td>
         <td width="100px"><a href="https://mediafire.com?${arr[i].dl}" target="_blank">Download</a></td>
-        <td><small>${arr[i].date}</small></td>
+        <td><samp>${arr[i].date}</samp></td>
         </tr>`);
     }
 }
