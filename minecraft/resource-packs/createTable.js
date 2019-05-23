07cbb6401965b602d89a2e3ebf3459e483664f1e
Nixinova <Nixinova@users.noreply.github.com>
2019-05-23 20:09:18 +1200

    Change spacing

--- minecraft/resource-packs/createTable.js
+++ minecraft/resource-packs/createTable.js
@@ -8,9 +8,8 @@ function createTable(id, arr) {
 
         table.append(`<tr>
         <td style="min-width: 250px">Nixinova Mash-Up ${arr[i].mcver} ${arr[i].id}</td>
-        <td>${arr[i].name}</td>
-        <td width="100px"></td>
-        <td width="100px"><a href="https://mediafire.com?${arr[i].dl}" target="_blank">Download</a></td>
+        <td width="100px">${arr[i].name}</td>
+        <td width="200px"><a href="https://mediafire.com?${arr[i].dl}" target="_blank">Download</a></td>
         <td><samp><time datetime="${arr[i].date.replace(/-/g,'')}+12:00">${arr[i].date}</time></samp></td>
         </tr>`);
     }
