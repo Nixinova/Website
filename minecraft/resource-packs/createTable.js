22486087b01177c8b36e177c21aba7972944c4b0
Nixinova <Nixinova@users.noreply.github.com>
2019-05-23 19:29:00 +1200

    Merge tables

--- minecraft/resource-packs/createTable.js
+++ minecraft/resource-packs/createTable.js
@@ -1,13 +1,13 @@
-function createTable(mcver, arr) {
-    var id = mcver.replace(/\./g, '_');
+function createTable(id, arr) {
     var table = $('#'+id);
 
     for (i in arr) {
         if (!arr[i].date) {arr[i].date = '(TBA)';}
         arr[i].name = arr[i].name ? arr[i].name.replace('v', '') : arr[i].packver;
+        if (arr[i].pre) {mcver += ` (pre${arr[i].pre})`;}
 
         table.append(`<tr>
-        <td style="min-width: 250px">Nixinova Mash-Up ${mcver} (${attr[i].mcver}) ${arr[i].id}</td>
+        <td style="min-width: 250px">Nixinova Mash-Up ${arr[i].mcver} ${arr[i].id}</td>
         <td>v${arr[i].name}</td>
         <td width="100px"><a href="https://mediafire.com?${arr[i].dl}" target="_blank">Download</a></td>
         <td><small>${arr[i].date}</small></td>
