e7557a2ffa52cfc45775e2ae12d16d68586a4fba
Nixinova <Nixinova@users.noreply.github.com>
2019-05-23 18:51:49 +1200

    min width

--- minecraft/resource-packs/createTable.js
+++ minecraft/resource-packs/createTable.js
@@ -4,7 +4,7 @@ function createTable(mcver, arr) {
     for (i in arr) {
         if (arr[i].date === undefined) {arr.date = '';}
         table.append(`<tr>
-        <td>Nixinova Mash-Up ${mcver}-${arr[i].id}</td>
+        <td style="min-width: 250px">Nixinova Mash-Up ${mcver}-${arr[i].id}</td>
         <td><a href="https://mediafire.com?${arr[i].dl}" target="_blank">Download</a></td>
         <td><small>${arr[i].date}</small></td>
         </tr>`);
