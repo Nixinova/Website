f05a482108b0f0b4d191c67ab0764820ab4999e7
Nixinova <Nixinova@users.noreply.github.com>
2019-05-23 19:10:59 +1200

    Fix var name

--- minecraft/resource-packs/createTable.js
+++ minecraft/resource-packs/createTable.js
@@ -2,12 +2,12 @@ function create_table(ver, arr) {
     var id = ver.replace(/\./g, '_');
     var table = $('#'+id);
     for (i in arr) {
-        if (arr[i].date === undefined) {arr.date = '';}
-        if (!attr[i].name) {attr[i].name = attr[i].packver;}
-        if (!attr[i].mcver) {attr[i].mcver = ver}
+        if (!arr[i].date) {arr[i].date = '';}
+        if (!attr[i].name) {arr[i].name = attr[i].packver;}
+        if (!attr[i].mcver) {arr[i].mcver = ver}
 
         table.append(`<tr>
-        <td style="min-width: 250px">Nixinova Mash-Up ${attr[i].mcver} ${arr[i].id}</td>
+        <td style="min-width: 250px">Nixinova Mash-Up ${arr[i].mcver} ${arr[i].id}</td>
         <td>v${attr[i].name}</td>
         <td><a href="https://mediafire.com?${arr[i].dl}" target="_blank">Download</a></td>
         <td><small>${arr[i].date}</small></td>
