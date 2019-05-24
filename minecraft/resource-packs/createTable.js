50a2551e9ab550ae97974e0deeb8c55af5e0c674
Nixinova <Nixinova@users.noreply.github.com>
2019-05-25 07:32:05 +1200

    Add widths

--- minecraft/resource-packs/createTable.js
+++ minecraft/resource-packs/createTable.js
@@ -8,8 +8,8 @@ function createTable(id, arr) {
         table.append(`<tr>
         <td style="min-width: 250px">Nixinova Mash-Up ${arr[i].mcver} ${arr[i].id}</td>
         <td width="100px">${arr[i].packver}</td>
-        <td width="200px"><a href="https://mediafire.com?${arr[i].dl}" target="_blank">Download</a></td>
-        <td><samp><time datetime="${arr[i].date.replace(/-/g,'')}+12:00">${arr[i].date}</time></samp></td>
+        <td width="150px"><a href="https://mediafire.com?${arr[i].dl}" target="_blank">Download</a></td>
+        <td width="100px"><samp><time datetime="${arr[i].date.replace(/-/g,'')}+12:00">${arr[i].date}</time></samp></td>
         </tr>`);
     }
 }
