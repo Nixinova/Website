88d2d3844e57632a7a240b77c87ec254f49c4d13
Nixinova <Nixinova@users.noreply.github.com>
2019-05-25 07:29:16 +1200

    Convert rest

--- minecraft/resource-packs/createTable.js
+++ minecraft/resource-packs/createTable.js
@@ -3,12 +3,11 @@ function createTable(id, arr) {
 
     for (i in arr) {
         if (!arr[i].date) {arr[i].date = '(TBC)';}
-        if (!arr[i].name) {arr[i].name = arr[i].packver;}
         if (arr[i].snap) {arr[i].mcver == `${arr[i].mcver} (${arr[i].snap})`;}
 
         table.append(`<tr>
         <td style="min-width: 250px">Nixinova Mash-Up ${arr[i].mcver} ${arr[i].id}</td>
-        <td width="100px">${arr[i].name}</td>
+        <td width="100px">${arr[i].packver}</td>
         <td width="200px"><a href="https://mediafire.com?${arr[i].dl}" target="_blank">Download</a></td>
         <td><samp><time datetime="${arr[i].date.replace(/-/g,'')}+12:00">${arr[i].date}</time></samp></td>
         </tr>`);
