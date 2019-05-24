828d185614d4073a3e3fbdda70173512763dc454
Nixinova <Nixinova@users.noreply.github.com>
2019-05-25 07:38:01 +1200

    Fix table spacing

--- minecraft/resource-packs/createTable.js
+++ minecraft/resource-packs/createTable.js
@@ -3,12 +3,12 @@ function createTable(id, arr) {
 
     for (i in arr) {
         if (!arr[i].date) {arr[i].date = '(TBC)';}
-        if (arr[i].snap) {arr[i].mcver == `${arr[i].mcver} (${arr[i].snap})`;}
+        if (arr[i].snap) {arr[i].mcver == `${arr[i].mcver}&ensp;(${arr[i].snap})`;}
 
         table.append(`<tr>
-        <td style="min-width: 250px">Nixinova Mash-Up ${arr[i].mcver} ${arr[i].id}</td>
-        <td width="100px">${arr[i].packver}</td>
-        <td width="150px"><a href="https://mediafire.com?${arr[i].dl}" target="_blank">Download</a></td>
+        <td width="250px">Nixinova Mash-Up ${arr[i].mcver} (${arr[i].id})</td>
+        <td width="060px">${arr[i].packver}</td>
+        <td width="100px"><a href="https://mediafire.com?${arr[i].dl}" target="_blank">Download</a></td>
         <td width="100px"><samp><time datetime="${arr[i].date.replace(/-/g,'')}+12:00">${arr[i].date}</time></samp></td>
         </tr>`);
     }
