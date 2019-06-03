4de64261581211f9e6c11af35487ed63d374799d
Nixinova <Nixinova@users.noreply.github.com>
2019-06-03 16:16:02 +1200

    Fix

--- minecraft/resource-packs/gallery.js
+++ minecraft/resource-packs/gallery.js
@@ -12,10 +12,10 @@ function togglePlay() {
 function gallery(arr) {
     if ($text.html() == 'pause' || count === 0) {
         const $gallery_img = $('#gallery .screenshot')
-        for (i in arr) {
-            let src = '/images/minecraft/resource-packs/' + arr[i];
-            $gallery_img.css("background-image", "url('" + src + "')");
-        }
+        let src = '/images/minecraft/resource-packs/' + arr[i];
+        $gallery_img.css("background-image", "url('" + src + "')");
     }
     count++;
+    i++;
+    if (i >= images.length) {i = 0;}
 };
