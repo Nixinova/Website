c1749fcf423d55e1971f4bc9061a5eed25b755e3
Nixinova <Nixinova@users.noreply.github.com>
2019-06-03 16:24:18 +1200

    Fix

--- minecraft/resource-packs/gallery.js
+++ minecraft/resource-packs/gallery.js
@@ -1,18 +1,8 @@
-var count = 0;
-
-function togglePlay() {
-    if ($text.html() == 'pause') {
-        $text.html('play_arrow');
-    } else {
-        $text.html('pause');
-    }
-}
-
 function gallery(arr) {
     const $gallery_img = $('#gallery .screenshot')
     let src = '/images/minecraft/resource-packs/' + arr[i];
     $gallery_img.css("background-image", "url('" + src + "')");
-    
+
     i++;
-    if (i >= images.length) {i = 0;}
+    if (i >= arr.length) {i = 0;}
 };
