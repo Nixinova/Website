0a3a87456cea05a189e5053e91ed94d52efca396
Nixinova <Nixinova@users.noreply.github.com>
2019-06-03 15:42:46 +1200

    Loop

--- minecraft/resource-packs/gallery.js
+++ minecraft/resource-packs/gallery.js
@@ -5,17 +5,18 @@ function gallery(arr) {
     if ($text.html() == 'pause' || count === 0) {
         const $gallery_img = $('#gallery .screenshot')
         const $caption = $('#caption p');
+        for (i in arr) {
+            if (arr[arr[i].path]) {arr[i].path = arr[arr[i].path].path}
+            
+            var src = '/images/minecraft/resource-packs/' + arr[i].path + '/' + arr[i].img;
 
-        if (arr[arr[i].path]) {arr[i].path = arr[arr[i].path].path}
-        
-        var src = '/images/minecraft/resource-packs/' + arr[i].path + '/' + arr[i].img;
+            $gallery_img.css("background-image", "url('" + src + "')").attr("alt", arr[i].caption);
+            $caption.html(arr[i].items.caption);
 
-        $gallery_img.css("background-image", "url('" + src + "')").attr("alt", arr[i].caption);
-        $caption.html(arr[i].items.caption);
-
-        i++;
-        if (i >= images.length) {i = 0;}
-        count++;
+            //i++;
+            //if (i >= images.length) {i = 0;}
+            //count++;
+        }
     }
 };
 
