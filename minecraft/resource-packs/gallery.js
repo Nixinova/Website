4d132ad9d6a3afae0203c4c858cbdbb53b68bd00
Nixinova <Nixinova@users.noreply.github.com>
2019-06-03 15:57:52 +1200

    Remove caption from gallery

--- minecraft/resource-packs/gallery.js
+++ minecraft/resource-packs/gallery.js
@@ -1,24 +1,4 @@
 var $text = $('#pause-text');
-var count = 0;
-
-function gallery(arr) {
-    if ($text.html() == 'pause' || count === 0) {
-        const $gallery_img = $('#gallery .screenshot')
-        const $caption = $('#caption p');
-        for (i in arr) {
-            //if (arr[arr[i].path]) {arr[i].path = arr[arr[i].path].path}
-            
-            let src = '/images/minecraft/resource-packs/' + arr[i].path + '/' + arr[i].img;
-
-            $gallery_img.css("background-image", "url('" + src + "')").attr("alt", arr[i].caption);
-            $caption.html(arr[i].caption);
-
-            //i++;
-            //if (i >= images.length) {i = 0;}
-            //count++;
-        }
-    }
-};
 
 function togglePlay() {
     if ($text.html() == 'pause') {
@@ -27,3 +7,13 @@ function togglePlay() {
         $text.html('pause');
     }
 }
+
+function gallery(arr) {
+    if ($text.html() == 'pause' || count === 0) {
+        const $gallery_img = $('#gallery .screenshot')
+        for (i in arr) {
+            let src = '/images/minecraft/resource-packs/' + arr[i];
+            $gallery_img.css("background-image", "url('" + src + "')");
+        }
+    }
+};
