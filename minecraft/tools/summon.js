20a4364aa4ea02da0626b175d37c563a788aa0b8
Nixinova <42429413+Nixinova@users.noreply.github.com>
2019-09-04 10:43:15 +1200

    Fix selectors

--- minecraft/tools/summon.js
+++ minecraft/tools/summon.js
@@ -89,8 +89,8 @@ function summon() {
 
     /// GENERATOR ///
     // OUTPUT //
-    $('#output_text').empty();
-    $('#cmd_note').addClass('hide');
+    $('#generator-output').empty();
+    $('#cmd-note').addClass('hide');
     $('.only').addClass('hide');
     $('.' + entity).removeClass('hide');
 
@@ -285,9 +285,9 @@ function summon() {
     const _ = ' ';
     var output = '/summon ' + entity + _ + X + _ + Y + _ + Z + _ + NBT;
     if (output.length > 256) {
-        $('#cmd_note').removeClass('hide');
+        $('#cmd-note').removeClass('hide');
    }
-    $('.generator-output').append(
+    $('#generator-output').append(
         '<span style="color: lightgray">/summon</span> ' +
         '<span style="color: #5ff">' + entity + '</span> ' +
         '<span style="color: #ff5">' + X + _ + Y + _ + Z + '</span> ' +
