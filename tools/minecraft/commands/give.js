ea7912b4d28bdd0a92e702540bd5dde5e92d127d
Nixinova <Nixinova@users.noreply.github.com>
2020-02-01 19:37:31 +1300

    Fix style

--- tools/minecraft/commands/give.js
+++ tools/minecraft/commands/give.js
@@ -501,7 +501,7 @@ function give() {
     let styled_NBT = NBT
         .replace(/&/g, '&amp;')
         .replace(/</g, '&lt;')
-        .replace(/\\\\u00A7([0-9a-f])/g, '<span class="§$1">§$1');
+        .replace(/\\\\u00A7([0-9a-f])/g, '</span><span class="§$1">§$1').replace(/(\"[,}])/, '</span>$1');
     $('#generator-output').html(`
         <span class="§7">/give</span>
         <span class="§b">${target_text + selector}</span>
