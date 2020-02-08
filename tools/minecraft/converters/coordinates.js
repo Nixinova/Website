f6864018d97115975f7ca3d2e97481ccb3603ae0
Nixinova <Nixinova@users.noreply.github.com>
2020-02-08 19:23:29 +1300

    Add nether region and fix conversions

--- tools/minecraft/converters/coordinates.js
+++ tools/minecraft/converters/coordinates.js
@@ -1,85 +1,111 @@
 function coords() {
     /// VARIABLES ///
     // call from input form //
-    var overworld_block_x = value('overworld_block_x');
-    var overworld_block_y = value('overworld_block_y');
-    var overworld_block_z = value('overworld_block_z');
-    var overworld_chunk_x = value('overworld_chunk_x');
-    var overworld_chunk_y = value('overworld_chunk_y');
-    var overworld_chunk_z = value('overworld_chunk_z');
-    var nether_block_x = value('nether_block_x');
-    var nether_block_y = value('nether_block_y');
-    var nether_block_z = value('nether_block_z');
-    var nether_chunk_x = value('nether_chunk_x');
-    var nether_chunk_y = value('nether_chunk_y');
-    var nether_chunk_z = value('nether_chunk_z');
-    var region_x = value('region_x');
-    var region_z = value('region_z');
+    var overworld_block_x  = value('overworld_block_x');
+    var overworld_block_y  = value('overworld_block_y');
+    var overworld_block_z  = value('overworld_block_z');
+    var overworld_chunk_x  = value('overworld_chunk_x');
+    var overworld_chunk_y  = value('overworld_chunk_y');
+    var overworld_chunk_z  = value('overworld_chunk_z');
+    var nether_block_x     = value('nether_block_x');
+    var nether_block_y     = value('nether_block_y');
+    var nether_block_z     = value('nether_block_z');
+    var nether_chunk_x     = value('nether_chunk_x');
+    var nether_chunk_y     = value('nether_chunk_y');
+    var nether_chunk_z     = value('nether_chunk_z');
+    var overworld_region_x = value('region_x');
+    var overworld_region_z = value('region_z');
 
     /// GENERATOR ///
 
     if (coordsType === 'overworld_block') {
-        nether_block_x    = Math.floor(overworld_block_x / 8);
-        nether_block_y    = overworld_block_y;
-        nether_block_z    = Math.floor(overworld_block_z / 8);
-        overworld_chunk_x = Math.floor(overworld_block_x / 16);
-        overworld_chunk_y = Math.floor(overworld_block_y / 16);
-        overworld_chunk_z = Math.floor(overworld_block_z / 16);
-        nether_chunk_x    = Math.floor(nether_block_x / 16);
-        nether_chunk_y    = Math.floor(nether_block_y / 16);
-        nether_chunk_z    = Math.floor(nether_block_z / 16);
-        region_x          = Math.floor(overworld_block_x / 512);
-        region_z          = Math.floor(overworld_block_z / 512);
+        nether_block_x     = Math.floor(overworld_block_x / 8);
+        nether_block_y     = overworld_block_y;
+        nether_block_z     = Math.floor(overworld_block_z / 8);
+        nether_chunk_x     = Math.floor(nether_block_x / 16);
+        nether_chunk_y     = Math.floor(nether_block_y / 16);
+        nether_chunk_z     = Math.floor(nether_block_z / 16);
+        overworld_chunk_x  = nether_chunk_x * 8;
+        overworld_chunk_y  = nether_chunk_y * 8;
+        overworld_chunk_z  = nether_chunk_z * 8;
+        overworld_region_x = Math.floor(overworld_block_x / 512);
+        overworld_region_z = Math.floor(overworld_block_z / 512);
+        nether_region_x    = Math.floor(overworld_region_x / 8);
+        nether_region_z    = Math.floor(overworld_region_z / 8);
     }
     else if (coordsType === 'overworld_chunk') {
-        overworld_block_x = overworld_chunk_x * 16;
-        overworld_block_y = overworld_chunk_y * 16;
-        overworld_block_z = overworld_chunk_z * 16;
-        nether_block_x    = overworld_chunk_x * 2;
-        nether_block_y    = overworld_chunk_y * 2;
-        nether_block_z    = overworld_chunk_z * 2;
-        region_x          = Math.floor(overworld_chunk_x / 32);
-        region_z          = Math.floor(overworld_chunk_z / 32);
+        overworld_block_x  = overworld_chunk_x * 16;
+        overworld_block_y  = overworld_chunk_y * 16;
+        overworld_block_z  = overworld_chunk_z * 16;
+        nether_block_x     = overworld_block_z / 8;
+        nether_block_y     = overworld_block_z / 8;
+        nether_block_z     = overworld_block_z / 8;
+        overworld_region_x = Math.floor(overworld_block_x / 512);
+        overworld_region_z = Math.floor(overworld_block_z / 512);
+        nether_region_x    = Math.floor(overworld_region_x / 8);
+        nether_region_z    = Math.floor(overworld_region_z / 8);
     }
     else if (coordsType === 'nether_block') {
-        overworld_block_x = nether_block_x * 8;
-        overworld_block_y = nether_block_y;
-        overworld_block_z = nether_block_z * 8;
-        overworld_chunk_x = Math.floor(nether_block_x / 2);
-        overworld_chunk_y = Math.floor(nether_block_x / 2);
-        overworld_chunk_z = Math.floor(nether_block_x / 2);
-        nether_chunk_x    = Math.floor(nether_block_x / 16);
-        nether_chunk_y    = Math.floor(nether_block_y / 16);
-        nether_chunk_z    = Math.floor(nether_block_z / 16);
-        region_x          = Math.floor(nether_block_x / 512);
-        region_z          = Math.floor(nether_block_z / 512);
+        overworld_block_x  = nether_block_x * 8;
+        overworld_block_y  = nether_block_y;
+        overworld_block_z  = nether_block_z * 8;
+        nether_chunk_x     = Math.floor(nether_block_x / 16);
+        nether_chunk_y     = Math.floor(nether_block_y / 16);
+        nether_chunk_z     = Math.floor(nether_block_z / 16);
+        overworld_chunk_x  = nether_chunk_x * 8;
+        overworld_chunk_y  = nether_chunk_y * 8;
+        overworld_chunk_z  = nether_chunk_z * 8;
+        overworld_region_x = Math.floor(overworld_block_x / 512);
+        overworld_region_z = Math.floor(overworld_block_z / 512);
+        nether_region_x    = Math.floor(overworld_region_x / 8);
+        nether_region_z    = Math.floor(overworld_region_z / 8);
     }
     else if (coordsType === 'nether_chunk') {
-        overworld_block_x = nether_chunk_x * 128;
-        overworld_block_y = nether_block_y;
-        overworld_block_z = nether_chunk_z * 128;
-        overworld_chunk_x = nether_chunk_x * 8;
-        overworld_chunk_y = nether_chunk_y * 8;
-        overworld_chunk_z = nether_chunk_z * 8;
-        nether_block_x    = Math.floor(nether_chunk_x / 16);
-        nether_block_y    = Math.floor(nether_chunk_y / 16);
-        nether_block_z    = Math.floor(nether_chunk_z / 16);
-        region_x          = Math.floor(nether_chunk_x / 32);
-        region_z          = Math.floor(nether_chunk_z / 32);
+        overworld_block_x  = nether_chunk_x * 128;
+        overworld_block_y  = nether_block_y;
+        overworld_block_z  = nether_chunk_z * 128;
+        nether_block_x     = Math.floor(nether_chunk_x / 16);
+        nether_block_y     = Math.floor(nether_chunk_y / 16);
+        nether_block_z     = Math.floor(nether_chunk_z / 16);
+        overworld_chunk_x  = nether_chunk_x * 8;
+        overworld_chunk_y  = nether_chunk_y * 8;
+        overworld_chunk_z  = nether_chunk_z * 8;
+        overworld_region_x = Math.floor(overworld_block_x / 512);
+        overworld_region_z = Math.floor(overworld_block_z / 512);
+        nether_region_x    = Math.floor(overworld_region_x / 8);
+        nether_region_z    = Math.floor(overworld_region_z / 8);
     }
     else if (coordsType === 'overworld_region') {
-        overworld_block_x = region_x * 128;
-        overworld_block_y = 0;
-        overworld_block_z = region_x * 128;
-        overworld_chunk_x = region_x * 32;
-        overworld_chunk_y = 0;
-        overworld_chunk_z = region_z * 32;
-        nether_block_x    = region_x * 8;
-        nether_block_y    = 0;
-        nether_block_z    = region_z * 8;
-        nether_chunk_x    = region_x * 4;
-        nether_chunk_y    = 0;
-        nether_chunk_z    = region_z * 4;
+        overworld_block_x  = overworld_region_x * 128;
+        overworld_block_y  = 0;
+        overworld_block_z  = overworld_region_x * 128;
+        overworld_chunk_x  = overworld_region_x * 32;
+        overworld_chunk_y  = 0;
+        overworld_chunk_z  = overworld_region_z * 32;
+        nether_block_x     = overworld_region_x * 8;
+        nether_block_y     = 0;
+        nether_block_z     = overworld_region_z * 8;
+        nether_chunk_x     = overworld_region_x * 4;
+        nether_chunk_y     = 0;
+        nether_chunk_z     = overworld_region_z * 4;
+        nether_region_x    = Math.floor(overworld_region_x / 8);
+        nether_region_z    = Math.floor(overworld_region_z / 8);
+    }
+    else if (coordsType === 'nether_region') {
+        overworld_region_x = nether_region_x * 8;
+        overworld_region_z = nether_region_x * 8;
+        overworld_block_x  = overworld_region_x * 128;
+        overworld_block_y  = 0;
+        overworld_block_z  = overworld_region_x * 128;
+        overworld_chunk_x  = overworld_region_x * 32;
+        overworld_chunk_y  = 0;
+        overworld_chunk_z  = overworld_region_z * 32;
+        nether_block_x     = overworld_region_x * 8;
+        nether_block_y     = 0;
+        nether_block_z     = overworld_region_z * 8;
+        nether_chunk_x     = overworld_region_x * 4;
+        nether_chunk_y     = 0;
+        nether_chunk_z     = overworld_region_z * 4;
     }
 
     /// OUTPUT ///
