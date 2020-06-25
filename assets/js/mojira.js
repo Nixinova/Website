b9db218eb7d8af78fdf213c3418d7f60e3352514
Nixinova <Nixinova@users.noreply.github.com>
2020-06-25 17:51:19 +1200

    Add version dropdown for navigation

--- assets/js/mojira.js
+++ assets/js/mojira.js
@@ -8,6 +8,7 @@ function start() {
     $('#loading').removeClass('hide');
     $('#information').html('');
     $('table tbody').html('');
+    location.hash = '';
 }
 
 function complete() {
@@ -23,6 +24,7 @@ function initial() {
 }
 
 function generateAllProjects() {
+    $('#navigation').addClass('hide');
     start();
     $('table thead').html(`
         <tr>
@@ -58,30 +60,37 @@ function generateProject(project) {
     start();
     $('table thead').html(`
         <tr>
-            <th style="max-width: 300px">Version</th>
-            <th style="width: 100px">Date</th>
+            <th style="max-width: 300px;">Version</th>
+            <th style="width: 100px;">Date</th>
             <th>Description</th>
-            <th>Quick Links</th>
+            <th style="width: 100px;">Quick Links</th>
         </tr>
     `);
     $.ajax({
         url: 'https://cors-anywhere.herokuapp.com/https://bugs.mojang.com/rest/api/2/project/' + project
     }).done(function (data) {
         //$('#information').html(data.description);
-        $('table thead').prepend(`<tr><td colspan=4> <a href="javascript:generateAllProjects()">&larr; Back</a> </td></tr>`);
+        $('table thead').prepend(`
+            <tr><td colspan=4 style="text-align: center;">
+                <a href="javascript:generateAllProjects()" style="position: relative; right: 10%;">&larr; Back</a>
+                <img src="${data.avatarUrls["48x48"]}"> ${data.name}
+            </td></tr>
+        `);
         for (let version of data.versions.reverse()) {
+            $('#dropdown').append(`\n<option>${version.name}</option>`)
             $('table tbody').append(`
-                <tr>
-                    <td style="max-width: 300px">${version.name}</td>
-                    <td style="width: 100px"><samp>${version.released && version.releaseDate || ''}</samp></td>
+                <tr id="${encodeURIComponent(version.name)}">
+                    <td style="max-width: 300px;">${version.name}</td>
+                    <td style="width: 100px;"><samp>${version.released && version.releaseDate || ''}</samp></td>
                     <td>${version.description || ''}</td>
-                    <td>
+                    <td style="width: 100px;">
                         <a href="${bugsLink('affectedVersion', version.name, project)}">Bugs</a>,
                         <a href="${bugsLink('fixVersion', version.name, project)}">Fixes</a>
                     </td>
                 </tr>
             `);
         }
+        $('#navigation').removeClass('hide');
         complete();
     });
 }
