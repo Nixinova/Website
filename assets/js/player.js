4ea8a3b61ce083e949d0427e985a76633f85d3bc
Nixinova <nixinova@gmail.com>
2021-10-16 17:38:04 +1300

    player-api: Convert result list to table

--- assets/js/player.js
+++ assets/js/player.js
@@ -1,17 +1,20 @@
+const API_ROOT = 'https://cors-anywhere.herokuapp.com/https://api.mojang.com';
 const sections = ['username', 'uuid', 'username-history', 'skin', 'cape'];
 const skinURLs = {}, capeURLs = {};
 
 function error(err) {
     if (err === 'PlayerNotFoundError') {
+        $('#username-table').addClass('hide');
         $('#username').empty();
         $('#uuid').empty();
-        $('#username-history').html('Could not find player');
         $('#skin').empty();
+        $('#error').html('Could not find player');
     } else if (err === 'SkinNotFoundError') {
         $('#skin').empty();
     } else {
-        $('output').html('Unknown error');
+        $('#error').html('Unknown error');
     }
+    complete();
     throw err;
 }
 
@@ -28,6 +31,7 @@ function complete() {
 }
 
 async function getInfo(playerName) {
+    $('.clearable').empty();
     $('#loading').removeClass('hide');
     $('output').addClass('hide');
     progress(0);
@@ -37,10 +41,7 @@ async function getInfo(playerName) {
     if (!playerName && query) playerName = query;
 
     // Retrieve player data
-    const playerData = await fetch(`https://cors-anywhere.herokuapp.com/https://api.mojang.com/users/profiles/minecraft/${playerName}`).then(data => data.json()).catch(() => {
-        complete();
-        error('PlayerNotFoundError');
-    });
+    const playerData = await fetch(`${API_ROOT}/users/profiles/minecraft/${playerName}`).then(data => data.json()).catch(() => error('PlayerNotFoundError'));
     progress(1 / 3);
     if (!playerData) error('PlayerNotFoundError');
     const uuid = playerData.id;
@@ -51,36 +52,43 @@ async function getInfo(playerName) {
     ].join('-');
 
     // Retrieve username data
-    const usernameData = await fetch(`https://cors-anywhere.herokuapp.com/https://api.mojang.com/user/profiles/${uuid}/names`).then(data => data.json()).catch(() => {
-        complete();
-        error('PlayerNotFoundError');
-    });
+    const usernameData = await fetch(`${API_ROOT}/user/profiles/${uuid}/names`).then(data => data.json()).catch(() => error('PlayerNotFoundError'));
     progress(2 / 3);
-    $('#username-history').empty();
     const username = usernameData[usernameData.length - 1].name;
     $('#username').html(username);
     $('#uuid').html(uuidFormatted);
+    $('#username-table').removeClass('hide');
+    const intl = (data = {}) => new Intl.DateTimeFormat('en-GB', { ...data, timeZone: 'UTC' });
+    const formatDate = stamp => stamp ? intl({ dateStyle: 'medium' }).format(stamp) : '';
+    const formatTime = stamp => stamp ? intl({ timeStyle: 'long' }).format(stamp).replace('UTC', '') : '';
+    const nameCounts = {};
     for (let i = 0; i < usernameData.length; i++) {
         const name = usernameData[i].name;
+        nameCounts[name] = nameCounts[name] + 1 || 1;
         const datestamp = usernameData[i].changedToAt;
-        const date = new Intl.DateTimeFormat('en-GB', { dateStyle: 'medium', timeStyle: 'long', timeZone: 'UTC' }).format(datestamp);
-        const dateHtml = `<time datetime="${datestamp ? new Date(datestamp).toISOString() : ''}">${date}</time>`
-        const nameHtml = `<strong>${name}</strong>`;
+        const nextDatestamp = usernameData[i + 1]?.changedToAt;
         const daysDiff = Math.round((new Date(usernameData[i + 1]?.changedToAt ?? new Date()) - datestamp) / 1e3 / 86400);
-        let historyEntry;
-        if (i === 0 && usernameData.length === 1) {
-            historyEntry = `<li>Current name: ${nameHtml}</li>`;
-        } else if (i === usernameData.length - 1) {
-            historyEntry = `<li>${nameHtml} (current)<br class="mobileonly"/> (${daysDiff} days; changed ${dateHtml})</li>`;
-        } else if (i === 0) {
-            historyEntry = `<li>${nameHtml}</li>`;
-        } else {
-            historyEntry = `<li>${nameHtml}<br class="mobileonly"/> (${daysDiff} days; changed ${dateHtml})</li>`;
-        }
-        $('#username-history').append(historyEntry);
+        $('#username-history').append(`<tr>
+            <td>${i + 1}</td>
+            <td>
+                <strong>${name}</strong>
+                <small>${nameCounts[name] > 1 ? `(#${nameCounts[name]})` : ''}</small>
+            </td>
+            <td title="${datestamp ? new Date(datestamp).toISOString() : ''}"">
+                ${formatDate(datestamp) || '(creation)'}<br>${formatTime(datestamp)}
+            </td>
+            <td title="${new Date(nextDatestamp ?? new Date()).toISOString()}">
+                ${nextDatestamp ? `${formatDate(nextDatestamp)}<br>${formatTime(nextDatestamp)}` : '(current)'}
+            </td>
+            <td>
+                ${!daysDiff ? '?' : daysDiff < 365 ? `${daysDiff} days` : `${Math.round(daysDiff / 365 * 10) / 10} years`}
+            </td>
+        </tr>`);
     }
 
-    const skinData = await fetch(`https://cors-anywhere.herokuapp.com/https://sessionserver.mojang.com/session/minecraft/profile/${uuid}`).then(data => data.json()).catch(() => {
+    let skinFetched = false;
+    const skinData = await fetch(`https://cors-anywhere.herokuapp.com/https://sessionserver.mojang.com/session/minecraft/profile/${uuid}`).then(data => data.json()).catch(() => skinFetched = true);
+    if (skinFetched) {
         complete();
         if (skinURLs[username] || capeURLs[username]) {
             $('#skin').html(`
@@ -93,13 +101,13 @@ async function getInfo(playerName) {
                     <img src="${capeURLs[username]}" alt="${username}'s cape">
                 `);
             }
-            throw 'Completed';
+            return;
         } else {
             error('SkinNotFoundError');
         }
-    });
-    let encoded_data = skinData.properties[0].value;
-    let decoded_data = JSON.parse(atob(encoded_data));
+    }
+    const encoded_data = skinData.properties[0].value;
+    const decoded_data = JSON.parse(atob(encoded_data));
     const skinURL = decoded_data.textures.SKIN.url.replace('http:', 'https:');
     if (username) skinURLs[username] = skinURL;
     $('#skin').html(`
