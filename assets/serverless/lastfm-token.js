849937611c7d5645c2a6cd415a7907be635e97f9
Nixinova <nixinova@gmail.com>
2023-09-14 16:00:07 +1200

    serverless/lastfm: Fix http statusCode key

--- assets/serverless/lastfm-token.js
+++ assets/serverless/lastfm-token.js
@@ -1,8 +1,10 @@
 const API_KEY = process.env.LASTFM_API_KEY;
 
-exports.handler = async (event) => {
+const handler = async (event) => {
     return {
-        status: 200,
+        statusCode: 200,
         body: JSON.stringify({token: API_KEY}),
     }
 };
+
+module.exports = { handler };
