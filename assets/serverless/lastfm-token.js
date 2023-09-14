1537405e9621a684ff26c3034c663327d8fb709b
Nixinova <nixinova@gmail.com>
2023-09-14 15:55:43 +1200

    serverless/lastfm: Fix variable name

--- assets/serverless/lastfm-token.js
+++ assets/serverless/lastfm-token.js
@@ -3,6 +3,6 @@ const API_KEY = process.env.LASTFM_API_KEY;
 exports.handler = async (event) => {
     return {
         status: 200,
-        body: JSON.stringify({token: LASTFM_API_KEY}),
+        body: JSON.stringify({token: API_KEY}),
     }
 };
