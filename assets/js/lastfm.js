fdef5145c5231eaf3c82a8496ca6f7ac3361ad32
Nixinova <nixinova@gmail.com>
2025-10-19 14:01:47 +1300

    lastfm: Avoid an extra fetch call

--- assets/js/lastfm.js
+++ assets/js/lastfm.js
@@ -93,7 +93,7 @@ async function getTaggedItems(username, tag) {
             collatedUrls.push(...urls);
 
             const attrs = data.taggings['@attr'];
-            if (attrs.page > attrs.totalPages) {
+            if (attrs.page >= attrs.totalPages) {
                 // This is the last page: exit
                 break;
             }
