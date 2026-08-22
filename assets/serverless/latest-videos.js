b7ad622f8f77cc6dc8915bb5a52aabbe978e363e
Nixinova <nixinova@gmail.com>
2026-08-22 22:53:18 +1200

    home: Move hardcoded limit out of lambda

--- assets/serverless/latest-videos.js
+++ assets/serverless/latest-videos.js
@@ -1,11 +1,9 @@
-const NUM_VIDEOS = 2;
 export default async () => {
     const url = 'https://www.youtube.com/feeds/videos.xml?channel_id=UCkBbTlppb8XHxI2rFryB1dQ';
     const xml = await fetch(url).then(x => x.text())
     const videoIds = [...xml.match(/(?<=videoId>).+(?=<\/)/g)];
-    const resultIds = videoIds.slice(0, NUM_VIDEOS);
 
-    return Response.json(resultIds, {
+    return Response.json(videoIds, {
         headers: {
             'Cache-Control': 'public, max-age=300'
         }
